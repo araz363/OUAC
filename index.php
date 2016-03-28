2d02c6c6a9d16dc0f0fbdf352fb494371a492f50
unknown <a.razafindramanana@BART-7510.estia.local>
2016-03-28 23:25:38 +0200

    - Put the database to unzip on amazon webservice

--- index.php
+++ index.php
@@ -4,19 +4,19 @@
 <head>
   <meta charset="UTF-8">
   <title>Once Upon a Cloud</title>
-  <link rel="stylesheet" href="../static/css/normalize.css">
-  <link rel="stylesheet" href="../static/css/index.css">
+  <link rel="stylesheet" href="./static/css/normalize.css">
+  <link rel="stylesheet" href="./static/css/index.css">
 </head>
 
 <body>
   <div class="wrapper">
-    <form method="post" name="form-login" class="login" action="./request/login.php">
+    <form method="post" name="form-login" class="login" action="./src/request/login.php">
       <p class="title">Log in</p>
       <input name="username" id="username" type="text" placeholder="username" autofocus/>
       <i class="fa fa-user"></i>
       <input name="password" id="password" type="password" placeholder="password" />
       <i class="fa fa-key"></i>
-      <p class="sign-up">New user ?<a href="./new_users/new_users.php"> Sign up</a></p>
+      <p class="sign-up">New user ?<a href="./src/new_users/new_users.php"> Sign up</a></p>
       <button id="login_btn">
         <i class="spinner"></i>
         <span class="state">Log in</span>
@@ -28,6 +28,6 @@
   <div class="ouac">
     <h1 class="brand">Once Upon A Cloud</h1>
   </div>
-  <script src="./index.js"></script>
+  <script src="./src/index.js"></script>
 </body>
 </html>
